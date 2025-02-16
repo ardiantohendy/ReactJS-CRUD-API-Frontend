4283841bb7bdb042789dfec9255ec64b3daae022
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-16 12:35:03 +0700

    adding table for data list

---
+++ src/components/Mainpage.jsx
@@ -0,0 +1,14 @@
+import TableContent from "./Table";
+
+function Mainpage() {
+  return (
+    <div className="container">
+      <div className="main">
+        <p>List of Books</p>
+        <TableContent />
+      </div>
+    </div>
+  );
+}
+
+export default Mainpage;
---
+++ src/components/Table.jsx
@@ -0,0 +1,24 @@
+function TableContent() {
+  return (
+    <>
+      <table className="table-books">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Published Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Empty...</td>
+            <td>Empty...</td>
+            <td>Empty...</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default TableContent;
